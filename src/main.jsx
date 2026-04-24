dfdccba43733a65eca7fcd48a72e53b490f5cefd
CarjoTv <anthonyolguinb@gmail.com>
2026-04-24 12:07:35 -0700

    Arreglo menor

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-window.addEventListener('error', (event) => {
-  console.error('Global error:', event.error);
-});
-
-window.addEventListener('unhandledrejection', (event) => {
-  console.error('Unhandled promise rejection:', event.reason);
-});
-
-const root = createRoot(document.getElementById('root'));
-
-root.render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
